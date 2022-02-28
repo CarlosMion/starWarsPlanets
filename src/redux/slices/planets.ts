edc8fcf9f04639501bb04db4113405970f5d493d
cmion-rbi <cmion@rbi.com>
2022-02-27 21:47:26 -0300

    finished seting up redux

--- src/redux/slices/planets.ts
+++ src/redux/slices/planets.ts
@@ -2,20 +2,27 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IPlanets, IPlanet } from 'api/planets/types';
 import { PlanetsState } from './types';
 
-const initialState: PlanetsState = {
-  data: { count: 0, next: '', previous: '', results: [] as IPlanet[] },
+const initialData = {
+  count: 0,
+  next: '',
+  previous: '',
+  results: [] as IPlanet[],
 };
 
-export const planetsSlice = createSlice({
+const initialState: PlanetsState = {
+  data: initialData,
+};
+
+const planetsSlice = createSlice({
   name: 'planets',
   initialState,
   reducers: {
-    incrementByAmount: (state, action: PayloadAction<IPlanets>) => {
+    updateData: (state, action: PayloadAction<IPlanets>) => {
       state.data = action.payload;
     },
   },
 });
 
-export const { incrementByAmount } = planetsSlice.actions;
+export const { updateData } = planetsSlice.actions;
 
 export default planetsSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import planetsReducer from './slices/planets';
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    planets: planetsReducer,
+  },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
