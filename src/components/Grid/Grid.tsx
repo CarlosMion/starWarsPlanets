8b5262b238a4f22dc141162c681bf929c2a63cea
cmion-rbi <cmion@rbi.com>
2022-02-28 13:38:11 -0300

    made action appearance optional in grid

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,40 +1,70 @@
-import React from 'react';
+import { IPlanet } from 'api/planets/types';
+import { Button } from 'components/Button';
+import { EditPlanet } from 'components/Dialogs/EditPlanet';
+import { useDialogModal } from 'hooks/useDialogModal';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { valueToKeyNamesMap } from 'utils/constants';
 
 import './Grid.css';
-import { IGridData } from './types';
+import { IAction, IGridData } from './types';
 
-export const Grid = ({ headers, values, actions }: IGridData) => {
+export const Grid = ({ headers, values, hasActions }: IGridData) => {
+  const [EditPlanetModal, openEditPlanetModal] = useDialogModal({
+    Component: EditPlanet,
+  });
+  const [selectedPlanet, setSelectedPlanet] = useState<IPlanet>();
+
+  const editPlanet = (planet: IPlanet) => {
+    setSelectedPlanet(planet);
+    console.log('planet', planet);
+    openEditPlanetModal();
+  };
+
   return (
-    <table className="gridTable">
-      <thead>
-        <tr>
-          {headers.map((colName) => (
-            <th key={colName}>{colName}</th>
-          ))}
-          {!!actions.length && <th>Actions</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {values.map((row, index) => (
-          <tr key={index}>
+    <>
+      <table className="gridTable">
+        <thead>
+          <tr>
             {headers.map((colName) => (
-              <td key={colName}>
-                {row[valueToKeyNamesMap.get(colName) ?? '']}
-              </td>
+              <th key={colName}>{colName}</th>
             ))}
-            {!!actions.length && (
-              <td className="gridActions">
-                {actions.map(({ label, action }) => (
-                  <button key={label} onClick={() => action(row)}>
-                    {label}
-                  </button>
-                ))}
-              </td>
-            )}
+            {hasActions && <th>Actions</th>}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {values.map((row, index) => (
+            <tr key={index}>
+              {headers.map((colName) => (
+                <td key={colName}>
+                  {row[valueToKeyNamesMap.get(colName) ?? '']}
+                </td>
+              ))}
+              {
+                <td className="gridActions">
+                  {!!row.actions.length &&
+                    row.actions.map((actionItem: IAction) => (
+                      <>
+                        {actionItem && (
+                          <Button
+                            key={actionItem.label}
+                            onClick={() => actionItem.action(row)}
+                            to={actionItem.to}
+                            as={actionItem.to ? Link : undefined}
+                          >
+                            {actionItem.label}
+                          </Button>
+                        )}
+                      </>
+                    ))}
+                  <Button onClick={() => editPlanet(row)}>Edit</Button>
+                </td>
+              }
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <EditPlanetModal planet={selectedPlanet} />
+    </>
   );
 };
--- src/components/Grid/types.ts
+++ src/components/Grid/types.ts
@@ -1,10 +1,11 @@
 export interface IAction {
   label: string;
   action: (row: any) => void;
+  to?: string;
 }
 
 export interface IGridData {
   headers: string[];
   values: any[];
-  actions: IAction[];
+  hasActions?: boolean;
 }
