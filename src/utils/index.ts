6f5f2f04da75a032382076d30bcb6b974aa4df06
cmion-rbi <cmion@rbi.com>
2022-03-02 01:26:32 -0300

    upgraded utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,80 @@
-import { IGridData } from 'components/Grid/types';
-import { keyToValueNamesMap } from './constants';
-import { IBuildPlanetsGridData } from './types';
+import { IFilm } from 'api/films/types';
+import { IPlanet } from 'api/planets/types';
+import { IResident } from 'api/residents/types';
+import { GridVariants, IGridData } from 'components/Grid/types';
+import {
+  IBuildPlanetFilmsData,
+  IBuildPlanetsGridData,
+  IBuildPlanetResidentsData,
+  IGetPlanetData,
+} from './types';
+
+const getActualTypeForValue = (potentialNumber: string) => {
+  const number: number = parseInt(potentialNumber);
+
+  return isNaN(number) ? 'string' : 'number';
+};
+
+const headerTypes = (
+  properties: {} & {
+    [x: string]: PropertyDescriptor;
+  }
+) => {
+  let arrayOfTypes: string[] = [];
+  Object.entries(properties).forEach(([_key, property]) => {
+    arrayOfTypes = [...arrayOfTypes, getActualTypeForValue(property.value)];
+  });
+
+  return arrayOfTypes;
+};
+
+const getHeaderNames = (data?: IPlanet | IFilm | IResident): string[] =>
+  Object.keys(data ? data : {});
+
+const getHeaderTypes = (data?: IPlanet | IFilm | IResident): string[] => {
+  const properties = Object.getOwnPropertyDescriptors(data ? data : {});
+  return headerTypes(properties);
+};
 
 export const buildPlanetsGridData = ({
   data,
 }: IBuildPlanetsGridData): IGridData => {
-  const { results } = data;
-  const rawHeaders = Object.keys(results ? results[0] : {});
-  const headers = rawHeaders.map(
-    (header) => keyToValueNamesMap.get(header) ?? ''
-  );
-  const onlyValidHeaders = headers.filter((header) => !!header);
+  const firstPlanet = data ? data[0] : ({} as IPlanet);
 
   return {
-    headers: onlyValidHeaders,
-    values: results ?? [],
+    headerNames: getHeaderNames(firstPlanet),
+    headerTypes: getHeaderTypes(firstPlanet),
+    values: data ?? [],
+    variant: GridVariants.PLANETS,
+  };
+};
+
+export const getPlanetData = ({
+  data,
+  planetName,
+}: IGetPlanetData): IPlanet | undefined =>
+  data?.find((planet: IPlanet) => planet.name === planetName);
+
+export const buildPlanetFilmsData = ({
+  film,
+  filmEndpoints,
+}: IBuildPlanetFilmsData): IGridData => {
+  return {
+    headerNames: getHeaderNames(film),
+    headerTypes: getHeaderTypes(film),
+    values: filmEndpoints ?? [],
+    variant: GridVariants.FILMS,
+  };
+};
+
+export const buildPlanetResidentsData = ({
+  resident,
+  residentEndpoints,
+}: IBuildPlanetResidentsData): IGridData => {
+  return {
+    headerNames: getHeaderNames(resident),
+    headerTypes: getHeaderTypes(resident),
+    values: residentEndpoints ?? [],
+    variant: GridVariants.RESIDENTS,
   };
 };
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,22 @@
-import { IPlanets } from 'api/planets/types';
+import { IFilm } from 'api/films/types';
+import { IPlanet } from 'api/planets/types';
+import { IResident } from 'api/residents/types';
 
 export interface IBuildPlanetsGridData {
-  data: IPlanets;
+  data?: IPlanet[];
+}
+
+export interface IGetPlanetData {
+  data?: IPlanet[];
+  planetName: string;
+}
+
+export interface IBuildPlanetFilmsData {
+  film?: IFilm;
+  filmEndpoints?: string[];
+}
+
+export interface IBuildPlanetResidentsData {
+  resident?: IResident;
+  residentEndpoints?: string[];
 }
