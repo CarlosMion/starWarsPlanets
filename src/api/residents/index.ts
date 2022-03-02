127caffd72086574c7482f71624761d5135388a4
cmion-rbi <cmion@rbi.com>
2022-03-02 01:31:30 -0300

    updated residents api files

---
+++ src/api/residents/index.ts
@@ -0,0 +1,1 @@
+export { useResident } from './residents';
---
+++ src/api/residents/residents.ts
@@ -0,0 +1,27 @@
+import { useQuery } from 'react-query';
+import { IResident, IResidentVariables } from './types';
+
+import { get, isResponseError } from '../http-client';
+import { RequestException } from 'api/types';
+
+async function getResident({ endpoint }: IResidentVariables) {
+  if (!endpoint) {
+    throw new Error('endpoint is missing!');
+  }
+
+  const result = await get({
+    endpoint,
+  });
+
+  if (isResponseError(result.status)) {
+    throw new RequestException(result as Response);
+  }
+  return result;
+}
+
+export function useResident({ endpoint }: IResidentVariables) {
+  const { data, isLoading } = useQuery<IResident>('planet_resident', () =>
+    getResident({ endpoint })
+  );
+  return { resident: data, isLoading };
+}
